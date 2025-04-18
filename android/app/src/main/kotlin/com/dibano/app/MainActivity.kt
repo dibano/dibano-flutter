edfbec9ae04e2ac76ea971c550436500fb50aeb4
Rahel Birrer <rahel.birrer@stud.hslu.ch>
2025-04-18 22:14:02 +0200

    fixed file path

---
+++ android/app/src/main/kotlin/com/dibano/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dibano.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/dibano/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dibano.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
