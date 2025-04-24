b8159edd356ed86f4fa7e565882c4c581d776cc0
Patrick Schöpfer <patrick.schoepfer@stud.hslu.ch>
2025-04-24 20:45:16 +0200

    Added Text to the information subpages

--- lib/ui/widgets/info_detail.dart
+++ lib/ui/widgets/info_detail.dart
@@ -1,11 +1,11 @@
 import 'package:dibano/ui/widgets/components/custom_app_bar.dart';
-import 'package:dibano/ui/widgets/components/custom_title.dart';
 import 'package:flutter/material.dart';
 
 class InfoDetail extends StatelessWidget {
-  const InfoDetail({super.key, required this.title});
+  const InfoDetail({super.key, required this.title, required this.text});
 
   final String title;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +14,8 @@ class InfoDetail extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 24),
-            CustomTitle(text: 'Beispieltext'),
             Text(
-              "Dies ist ein Beispieltext für Ihre Informationsseite. Hier können Sie wichtige Details und Neuigkeiten zu Ihrem Projekt oder Unternehmen einfügen. Passen Sie diesen Text nach Ihren Bedürfnissen an und fügen Sie relevante Informationen hinzu, um Ihre Nutzer auf dem Laufenden zu halten.",
+              text,
               textAlign: TextAlign.center,
             ),
           ],
